1af922bf2d5f1921d91ebb95c2d2c3ec9a0d2260
jonathan zollinger <jonathan.zollinger@outlook.com>
2024-09-14 21:24:54 -0600

    feat: create utils class, convert file to record

--- src/main/java/com/remote_guardian/MoveCommand.java
+++ src/main/java/com/remote_guardian/MoveCommand.java
@@ -4,35 +4,15 @@ import io.micronaut.configuration.picocli.PicocliRunner;
 
 import picocli.CommandLine.Command;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.security.MessageDigest;
-import java.util.Arrays;
-
 @Command(name = "move", description = "...",
         mixinStandardHelpOptions = true)
 public class MoveCommand implements Runnable {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         PicocliRunner.run(MoveCommand.class, args);
     }
 
     public void run() {
-
-    }
-
-    void HashFile(MessageDigest messageDigest, String... filePath) {
-        Arrays.stream(filePath).forEach(
-            path -> {
-                try (InputStream inputStream = Files.newInputStream(Paths.get(path))) {
-
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        );
-
+        // do stuff
     }
 }
