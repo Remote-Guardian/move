f202d3a5f83208bbd8ce7e6f79b658c847e14a5d
jonathan zollinger <jonathan.zollinger@outlook.com>
2024-09-16 19:50:26 -0600

    test: introduce rgexception class tests

---
+++ src/test/groovy/com/remote_guardian/RemoteGuardianExceptionTest.groovy
@@ -0,0 +1,37 @@
+package com.remote_guardian
+
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import spock.lang.Specification
+
+@MicronautTest
+class RemoteGuardianExceptionTest extends Specification {
+
+    void "test RemoteGuardianException"() {
+        when:
+        try {
+            Math.divideExact(1,0)
+        } catch (Exception e) {
+            throw new RemoteGuardianException("this is a test", e)
+        }
+
+        then:
+        def error = thrown(RemoteGuardianException)
+        error.message == "this is a test"
+        error.cause.class == ArithmeticException.class
+    }
+
+    void "test RemoteGuardianException with message"() {
+        when:
+        try {
+            Math.divideExact(1,0)
+        } catch (Exception ignored) {
+            throw new RemoteGuardianException("this is a test")
+        }
+
+        then:
+        def error = thrown(RemoteGuardianException)
+        error.message == "this is a test"
+        null == error.cause
+
+    }
+}
