4cd689728e3d1e256ce87899af144d10327b68eb
jonathan zollinger <jonathan.zollinger@outlook.com>
2024-09-25 20:45:44 -0600

    fix(test): swap exception production types
    locally testing off graalvm jdk causes errors when
    ci is testing against zulu jdk. change all to temurin.

--- core/src/test/groovy/com/remoteguardian/RemoteGuardianExceptionSpec.groovy
+++ core/src/test/groovy/com/remoteguardian/RemoteGuardianExceptionSpec.groovy
@@ -9,7 +9,7 @@ class RemoteGuardianExceptionSpec extends Specification {
     void "test RemoteGuardianException"() {
         when:
         try {
-            Math.divideExact(1,0)
+            new java.io.File(null as String)
         } catch (Exception e) {
             throw new RemoteGuardianException("this is a test", e)
         }
@@ -17,13 +17,13 @@ class RemoteGuardianExceptionSpec extends Specification {
         then:
         def error = thrown(RemoteGuardianException)
         error.message == "this is a test"
-        error.cause.class == ArithmeticException.class
+        error.cause.class == NullPointerException.class
     }
 
     void "test RemoteGuardianException with message"() {
         when:
         try {
-            Math.divideExact(1,0)
+            new java.io.File(null as String)
         } catch (Exception ignored) {
             throw new RemoteGuardianException("this is a test")
         }
